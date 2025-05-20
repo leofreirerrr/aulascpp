21223587507e161706edfe17297e757f7af2b1b2
Leonardo Freire <leofreirerr@gmail.com>
2025-05-20 10:10:53 -0300

    Aula 20/05
    Alterando valor de uma variavel do tipo struct por referencia

--- struct.cpp
+++ struct.cpp
@@ -4,15 +4,27 @@ using namespace std;
 struct country{
     string nome;
     int populacao;
-};
+};//ponto e virgula é obrigatorio
 
+void alterar(country &p1){
+    cout<<"Digite um novo nome: ";
+    cin>>p1.nome;
+}
+
+void imprime(country &p1){
+    cout<<"País: "<<p1.nome<<"\t"<<"População: "<<p1.populacao<<endl;
+}
 int main(){
     country p1;
     
     cout<<"Insira o nome e a população do país: ";
     cin>>p1.nome;
     cin>>p1.populacao;
-    cout<<"País: "<<p1.nome<<"\t"<<"População: "<<p1.populacao<<endl;
+
+    imprime(p1);
+    alterar(p1);
+    imprime(p1);
+
 
     return 0;
 }
